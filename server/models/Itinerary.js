269ac8a9639d00c0bc6c8a8937f0382a4699e4d5
sashuttle <sashuttle25@outlook.com>
2024-06-28 12:54:28 -0500

    added models and routes

---
+++ server/models/Itinerary.js
@@ -0,0 +1,12 @@
+//Mongoose model for itineraries that references a user, a title, activities, and a date. This will be associated with a specific user.
+//Fixme: May need to change the ItinerarySchema based on what we are looking to do(do we want a list of activities, etc?)
+const mongoose = require('mongoose');
+
+const ItinerarySchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    title: {type: String, required:true},
+    activities: [{type: String}],
+    date: {type: Date, required: true},
+});
+
+module.exports = mongoose.model('Itinerary', ItinerarySchema);
