a488a8d972a8a96316ffe047af7015dfb3501d76
Francdeve <156461955+fgsdeve@users.noreply.github.com>
2024-07-05 11:17:22 -0600

    added home, profile, itinerary codes in the pages folder

---
+++ client/src/pages/Itinerary.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ItineraryList from '../components/Itinerary/ItineraryList';
+
+const Itinerary = () => {
+    return (
+        <div>
+            <h1>Your Itineraries</h1>
+            <ItineraryList/>
+        </div>
+    )
+};
+
+export default Itinerary;
