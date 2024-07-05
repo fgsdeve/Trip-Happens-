53b5456b73c6d56aef0193b73633665b0d8021d3
sashuttle <sashuttle25@outlook.com>
2024-07-05 11:39:19 -0500

    components code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BroserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Itinerary from './pages/Itinerary';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route exact="true" path="/" component= {Home} />
+          <Route path="/itinerary" component={Itinerary} />
+          <Route path="/profile" component={Profile} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
