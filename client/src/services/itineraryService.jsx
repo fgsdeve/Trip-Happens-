c271f5ee764dba71a6c3c9797f1a687776529738
Francdeve <156461955+fgsdeve@users.noreply.github.com>
2024-07-20 08:47:26 -0500

    fecthing the latest change from Sam branch

--- client/src/services/itineraryService.jsx
+++ client/src/services/itineraryService.jsx
@@ -1,19 +1,33 @@
 import axios from 'axios';
 
-const API_KEY = 'd875bf90b3msh5322d0212594c58p18188djsnf6a23b67e4d7';
-const API_HOST = 'booking-com.p.rapidapi.com';
+const API_URL = '/api/itineraries/';
+
+const getItineraries = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+const createItinerary = async (itineraryData) => {
+  const response = await axios.post(API_URL, itineraryData);
+  return response.data;
+};
+
+const getItinerary = async (id) => {
+  const response = await axios.get(API_URL + id);
+  return response.data;
+};
 
 const getNearbyPlaces = async (hotelId, locale = 'en-gb') => {
   const options = {
     method: 'GET',
-    url: `https://${API_HOST}/v1/hotels/nearby-places`,
+    url: `https://booking-com.p.rapidapi.com/v1/hotels/nearby-places`,
     params: {
       hotel_id: hotelId,
       locale: locale,
     },
     headers: {
-      'x-rapidapi-key': API_KEY,
-      'x-rapidapi-host': API_HOST,
+      'x-rapidapi-key': 'd875bf90b3msh5322d0212594c58p18188djsnf6a23b67e4d7',
+      'x-rapidapi-host': 'booking-com.p.rapidapi.com',
     },
   };
 
@@ -26,4 +40,4 @@ const getNearbyPlaces = async (hotelId, locale = 'en-gb') => {
   }
 };
 
-export { getNearbyPlaces };
+export { getItineraries, createItinerary, getItinerary, getNearbyPlaces };
